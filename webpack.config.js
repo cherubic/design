56740d116ac136cbc0b8e5710b42b7d8d3cf88fa
Xander <copyrightcherubic@gmail.com>
2023-03-08 00:39:01 +0800

    demo project

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,32 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+  entry: {
+    index: ['./index.js', './package/header/header.sass']
   },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
+  },
+  mode: 'development',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    port: 9000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+  }),
+  ],
   module: {
     rules: [
       {
